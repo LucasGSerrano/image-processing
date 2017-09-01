4a74902ee6b3f99831300ef59bf19a656b16a5b9
Lucas Serrano <lucasgserrano@hotmail.com>
2017-08-31 23:25:34 -0300

    hsi_to_rgb

--- image_processing.m
+++ image_processing.m
@@ -36,7 +36,35 @@ img_hsi(:,:,2)=S;
 img_hsi(:,:,3)=I;
 figure,imshow(img_hsi);title('HSI');
 %% HSI To RGB
-%img_hsi = rgb2hsi(img);
+% Implement the conversion equations.
+R = zeros(size(img_hsi, 1), size(img_hsi, 2));
+G = zeros(size(img_hsi, 1), size(img_hsi, 2));
+B = zeros(size(img_hsi, 1), size(img_hsi, 2));
+
+% RG sector (0 <= H < 2*pi/3).
+idx = find( (0 <= H) & (H < 2*pi/3));
+B(idx) = I(idx) .* (1 - S(idx));
+R(idx) = I(idx) .* (1 + S(idx) .* cos(H(idx)) ./ ...
+                                          cos(pi/3 - H(idx)));
+G(idx) = 3*I(idx) - (R(idx) + B(idx));
+
+% BG sector (2*pi/3 <= H < 4*pi/3).
+idx = find( (2*pi/3 <= H) & (H < 4*pi/3) );
+R(idx) = I(idx) .* (1 - S(idx));
+G(idx) = I(idx) .* (1 + S(idx) .* cos(H(idx) - 2*pi/3) ./ cos(pi - H(idx)));
+B(idx) = 3*I(idx) - (R(idx) + G(idx));
+
+% BR sector.
+idx = find( (4*pi/3 <= H) & (H <= 2*pi));
+G(idx) = I(idx) .* (1 - S(idx));
+B(idx) = I(idx) .* (1 + S(idx) .* cos(H(idx) - 4*pi/3) ./ cos(5*pi/3 - H(idx)));
+R(idx) = 3*I(idx) - (G(idx) + B(idx));
+
+% Combine all three results into an RGB image.  Clip to [0, 1] to
+% compensate for floating-point arithmetic rounding effects.
+img_hsi_rgb = cat(3, R, G, B);
+img_hsi_rgb = max(min(img_hsi_rgb, 1), 0);
+figure,imshow(img_hsi_rgb);title('RGB - HSI');
 %imshow(img_hsi);
 %% RGB To HSV
 img_hsv = rgb2hsv(img);
